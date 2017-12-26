168fdef86ff9a521c8e596e2a93a8efe9ece5188
FlamingFireball64 <flamingfireball64@gmail.com>
2017-12-26 20:43:38 +0000

    Update emoji.js

--- emoji.js
+++ emoji.js
@@ -1,10 +1,8 @@
 asemojis.createEmojis = function () {
-  var counter = 0;
   var emojiPlaceholders = document.getElementsByClassName("asEmojiPlaceholder");
-  while (counter < emojiPlaceholders.length) {
+  while (0 < emojiPlaceholders.length) {
     var categoryID = emojiPlaceholders[0].getElementsByTagName("span")[0].innerHTML;
     var emojiID = emojiPlaceholders[0].getElementsByTagName("span")[1].innerHTML;
     emojiPlaceholders[0].outerHTML = "<a href='http://emojis.appstone.net' target='_blank' title='AppStone Emoji - " + asemojis.emojiData[categoryID].name + " - " + asemojis.emojiData[categoryID].items[emojiID].name + "'><img src='http://emojis.appstone.net/icons/" + asemojis.emojiData[categoryID].path + "/" + asemojis.emojiData[categoryID].items[emojiID].path + "' class='appstoneEmoji' alt='-" + asemojis.emojiData[categoryID].items[emojiID].name + "-' /></a>";
-    counter = counter + 1;
   }
 }
