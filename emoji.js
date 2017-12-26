992fabe4f22c40add45ae3cb3556b7876fe8b10d
FlamingFireball64 <flamingfireball64@gmail.com>
2017-12-26 20:33:29 +0000

    Update emoji.js

--- emoji.js
+++ emoji.js
@@ -1,14 +1,8 @@
-function emojiSrc(categoryID, emojiID) {
-  return "<a href='http://emojis.appstone.net' target='_blank' title='AppStone Emoji - " + emojis[categoryID].name + " - " + emojis[categoryID].items[emojiID].name + "'><img src='http://emojis.appstone.net/icons/" + emojis[categoryID].path + "/" + emojis[categoryID].items[emojiID].path + "' class='appstoneEmoji' alt='-" + emojis[categoryID].items[emojiID].name + "-' /></a>";
-}
-
-function createEmojis() {
+asemojis.createEmojis = function () {
   var counter = 0;
   var emojiPlaceholders = document.getElementsByClassName("asEmojiPlaceholder");
   while (counter < emojiPlaceholders.length) {
-    emojiPlaceholders[counter].innerHTML = emojiPlaceholders[counter].innerHTML + emojiSrc(emojiPlaceholders[counter].getElementsByTagName("span")[0].innerHTML, emojiPlaceholders[counter].getElementsByTagName("span")[1].innerHTML);
-    emojiPlaceholders[counter].getElementsByTagName("span")[0].style.display = "none";
-    emojiPlaceholders[counter].getElementsByTagName("span")[1].style.display = "none";
+    emojiPlaceholders[0].outerHTML = "<a href='http://emojis.appstone.net' target='_blank' title='AppStone Emoji - " + asemojis.emojiData[categoryID].name + " - " + asemojis.emojiData[categoryID].items[emojiID].name + "'><img src='http://emojis.appstone.net/icons/" + asemojis.emojiData[categoryID].path + "/" + asemojis.emojiData[categoryID].items[emojiID].path + "' class='appstoneEmoji' alt='-" + asemojis.emojiData[categoryID].items[emojiID].name + "-' /></a>";
     counter = counter + 1;
   }
 }
