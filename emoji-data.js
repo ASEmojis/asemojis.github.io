052ac5d642152610bd6223a1c8ce36f4dfd32332
FlamingFireball64 <flamingfireball64@gmail.com>
2017-12-26 20:30:09 +0000

    Update emoji-data.js

--- emoji-data.js
+++ emoji-data.js
@@ -1,98 +1,100 @@
-var emojis = [{
-  "name" : "Pencil and Paper",
-  "path" : "pencil-and-paper",
-  "items" : [{
-    "name" : "Smiley",
-    "path" : "smiley.png"
-  }, {
-    "name" : "Sad",
-    "path" : "sad.png"
-  }, {
-    "name" : "Smiley2",
-    "path" : "smiley2.png"
-  }, {
-    "name" : "Sad2",
-    "path" : "sad2.png"
-  }, {
-    "name" : "Angry Smile",
-    "path" : "angrysmile.png"
-  }, {
-    "name" : "Worried",
-    "path" : "worried.png"
-  }, {
-    "name" : "Unsure",
-    "path" : "unsure.png"
-  }, {
-    "name" : "What?",
-    "path" : "what.png"
-  }, {
-    "name" : "Party Time!",
-    "path" : "party.png"
-  }, {
-    "name" : "Cake",
-    "path" : "cake.gif"
-  }, {
-    "name" : "Bomb",
-    "path" : "bomb.gif"
-  }, {
-    "name" : "Grumpy Cloud",
-    "path" : "grumpycloud.gif"
-  }, {
-    "name" : "No",
-    "path" : "no.png"
-  }, {
-    "name" : "Yes",
-    "path" : "yes.png"
-  }, {
-    "name" : "Sleeping",
-    "path" : "sleeping.gif"
-  }, {
-    "name" : "Dead",
-    "path" : "dead.png"
-  }, {
-    "name" : "Shocked",
-    "path" : "shocked.png"
-  }, {
-    "name" : "Happy Sun",
-    "path" : "happysun.png"
-  }, {
-    "name" : "Rainbow",
-    "path" : "rainbow.png"
-  }, {
-    "name" : "Wine",
-    "path" : "wine.png"
-  }, {
-    "name" : "Danger!",
-    "path" : "danger.png"
-  }, {
-    "name" : "Lightning",
-    "path" : "lightning.gif"
-  }, {
-    "name" : "Ghost",
-    "path" : "ghost.gif"
-  }, {
-    "name" : "Torch",
-    "path" : "torch.gif"
-  }, {
-    "name" : "Candle",
-    "path" : "candle.gif"
-  }, {
-    "name" : "Ice Cream",
-    "path" : "icecream.png"
-  }, {
-    "name" : "Jar",
-    "path" : "jar.png"
-  }, {
-    "name" : "Tree",
-    "path" : "tree.png"
-  }, {
-    "name" : "Flower",
-    "path" : "flower.png"
-  }, {
-    "name" : "Book",
-    "path" : "book.png"
-  }, {
-    "name" : "Pencil",
-    "path" : "pencil.png"
+var asemojis = {
+  "emojiData" : [{
+    "name" : "Pencil and Paper",
+    "path" : "pencil-and-paper",
+    "items" : [{
+      "name" : "Smiley",
+      "path" : "smiley.png"
+    }, {
+      "name" : "Sad",
+      "path" : "sad.png"
+    }, {
+      "name" : "Smiley2",
+      "path" : "smiley2.png"
+    }, {
+      "name" : "Sad2",
+      "path" : "sad2.png"
+    }, {
+      "name" : "Angry Smile",
+      "path" : "angrysmile.png"
+    }, {
+      "name" : "Worried",
+      "path" : "worried.png"
+    }, {
+      "name" : "Unsure",
+      "path" : "unsure.png"
+    }, {
+      "name" : "What?",
+      "path" : "what.png"
+    }, {
+      "name" : "Party Time!",
+      "path" : "party.png"
+    }, {
+      "name" : "Cake",
+      "path" : "cake.gif"
+    }, {
+      "name" : "Bomb",
+      "path" : "bomb.gif"
+    }, {
+      "name" : "Grumpy Cloud",
+      "path" : "grumpycloud.gif"
+    }, {
+      "name" : "No",
+      "path" : "no.png"
+    }, {
+      "name" : "Yes",
+      "path" : "yes.png"
+    }, {
+      "name" : "Sleeping",
+      "path" : "sleeping.gif"
+    }, {
+      "name" : "Dead",
+      "path" : "dead.png"
+    }, {
+      "name" : "Shocked",
+      "path" : "shocked.png"
+    }, {
+      "name" : "Happy Sun",
+      "path" : "happysun.png"
+    }, {
+      "name" : "Rainbow",
+      "path" : "rainbow.png"
+    }, {
+      "name" : "Wine",
+      "path" : "wine.png"
+    }, {
+      "name" : "Danger!",
+      "path" : "danger.png"
+    }, {
+      "name" : "Lightning",
+      "path" : "lightning.gif"
+    }, {
+      "name" : "Ghost",
+      "path" : "ghost.gif"
+    }, {
+      "name" : "Torch",
+      "path" : "torch.gif"
+    }, {
+      "name" : "Candle",
+      "path" : "candle.gif"
+    }, {
+      "name" : "Ice Cream",
+      "path" : "icecream.png"
+    }, {
+      "name" : "Jar",
+      "path" : "jar.png"
+    }, {
+      "name" : "Tree",
+      "path" : "tree.png"
+    }, {
+      "name" : "Flower",
+      "path" : "flower.png"
+    }, {
+      "name" : "Book",
+      "path" : "book.png"
+    }, {
+      "name" : "Pencil",
+      "path" : "pencil.png"
+    }]
   }]
-}]
+};
